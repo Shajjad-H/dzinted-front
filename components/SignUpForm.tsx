e208def10ae02a85c5d5c1ed3c39118278674f48
YacineBoukliff <yacine69800@live.fr>
2025-05-12 23:40:41 +0200

    Projet pret avec archi et début mise en page demain

---
+++ components/SignUpForm.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SignUpForm = () => {
+  return (
+    <View>
+      <Text>SignUpForm</Text>
+    </View>
+  )
+}
+
+export default SignUpForm
